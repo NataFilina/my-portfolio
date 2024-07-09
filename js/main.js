253c852d7e858dec590620bf837ba0994faad19d
NataFilina <filinanatash@gmail.com>
2024-07-09 14:13:03 +0300

    added languages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tabs = document.querySelectorAll(".tabs_item");
+const tabs = document.querySelectorAll(".tabs_item");
 
-  tabs.forEach((elem) => {
-    elem.addEventListener("click", () => {
-      elem.classList.toggle("tabs_item_active");
-    });
+tabs.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    elem.classList.toggle("tabs_item_active");
   });
 });
+
 $(".variable-width").slick({
   dots: true,
   infinite: true,
